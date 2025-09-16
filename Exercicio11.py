31d5eeb8d92a6fc2387169be26ef98d769472a14
luizcjunior2010 <luizcjunior2010@hotmail.com>
2025-09-15 21:51:45 -0300

    Add files via upload

---
+++ Exercicio11.py
@@ -0,0 +1,5 @@
+# Use o for para imprimir todos os números pares de 2  a 20.
+for numero in range(2,20):
+
+    numero % 2 == 0 and print(numero)
+print("Contagem finalizada.")
---
+++ Exercicio13.py
@@ -0,0 +1,5 @@
+# Iprima a taboada do 7 (do até o 10) usando o for.
+for numero in range(1, 11):
+    resultado = 7 * numero
+    print(resultado)
+print("Tabuada Impressa")
